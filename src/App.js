50ae7511b58bd27b3e2790022722467bc49e5099
Sayo <sl.deve080412@gmail.com>
2021-06-25 18:08:32 -0700

    I finished MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
+import styled from "styled-components";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then(({ data }) => setData(data))
+      .catch((err) => console.log(err));
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map((d, index) => {
+        return <Character data={d} key={index} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,35 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+  const { data } = props;
+  //   console.log(data);
+  return (
+    <Information>
+      <Name>🚀 {data.name} 👾</Name>
+      <h3>Gendar: {data.gender}</h3>
+      <h3>Height: {data.height}</h3>
+      <h3>Mass: {data.mass}</h3>
+      <h3>Birth Year:{data.birth_year}</h3>
+      <h3>Eye Color: {data.eye_color}</h3>
+      <h3>Hair Color: {data.hair_color}</h3>
+      <h3>Skin Color: {data.skin_color}</h3>
+    </Information>
+  );
+};
+
+export default Character;
+
+const Information = styled.h2`
+  background-color: black;
+  color:white;
+  filter: blur
+  height: 30%;
+  width: 33%;
+  margin: 5px auto;
+`;
+
+const Name = styled.h2`
+color: yellow;
+`
